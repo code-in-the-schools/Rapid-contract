29c643d7b13549b1fca954abd73964e10013a1b7
LizvethM <morales-garciaLizveth2021@email.com>
2020-07-22 15:30:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#user input
+num = int(input(""))
+#if statements
+
+if (num== 1):
+  print("This animal has 244 bones! ")
+elif(num==2):
+  print("They are genetically 95.6% tiger! ")
+elif(num==3):
+  print("They can run around 30 mph! ")
+else:
+  print("They spend 70% of their lives sleeping! ")
